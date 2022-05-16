95e0b988989447746fb40edc94d4efd54f373a3c
Mahesh <maheshsainni@gmail.com>
2022-05-16 18:22:38 +0530

    hook_page_attachments removed

--- js/jwt.js
+++
@@ -1,9 +0,0 @@
-(function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.UserJWT = {
-    attach: function (context, settings) {
-      $(document).ready(function() {
-        console.log('User JWT : ', drupalSettings);
-      });
-    }
-  };
-}(jQuery, Drupal, drupalSettings));
